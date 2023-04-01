2ceb459466e333c62ec5fe3e9ea058bc2a8e9a33
ashura <ghanatva@gmail.com>
2023-04-01 10:13:29 +0530

    pattern_matching

---
+++ pattern_matching.py
@@ -0,0 +1,23 @@
+metro_areas = [
+('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+('São Paulo', 'BR', 19.649, (-23.547778, -46.635833)),
+]
+
+def main():
+    print(f'{"":15} | {"lattitude":>9} | {"longitude":>9}')
+
+    for record in metro_areas:
+        match record:
+            case [name,_,_,(lat,lon)] if lon <=0 :
+                print(f'{name:15} | {lat:9.4f} | {lon:9.4f}')
+            #pattern matching using *_ and strict data types 
+            #both cases works fine but in output only first one is printed
+
+            case [str(name),*_,(float(lat),float(lon))] if lon<=0:
+                print(f'{name:15} | {lat:9.4f} | {lon:9.4f}')
+
+if __name__=='__main__':
+    main()
