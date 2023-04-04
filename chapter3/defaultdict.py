dcdbe028aa1552bc1b13ef5028172ce07bb6ea3b
ashura <ghanatva@gmail.com>
2023-04-04 12:02:37 +0530

    missing key handelling

---
+++ chapter3/defaultdict.py
@@ -0,0 +1,16 @@
+import collections
+import re
+import sys
+
+WORD_RE=re.compile(r'\w+')
+index=collections.defaultdict(list)
+with open(sys.argv[0],encoding='utf-8') as fp:
+    for line_no,line in enumerate(fp,1):
+        for match in WORD_RE.finditer(line):
+            word=match.group()
+            column_no=match.start()+1
+            location = (line,column_no)
+            index[word].append(location)
+
+for word in sorted(index,key=str.upper):
+    print(word,index[word])
