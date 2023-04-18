74a2edf20768860837be51d001f9dc88b2f4d0ac
ashura <ghanatva@gmail.com>
2023-04-18 19:20:20 +0530

    docstring

---
+++ chapter3/docstrings.py
@@ -0,0 +1,17 @@
+def add(x,y):
+
+    '''
+        This function takes two numbers and finds their sum.
+    '''
+    print("sum=",x+y)
+
+def message():
+    '''
+    Use the command 
+    python -m pydoc -w ex
+    -m for module -w for instructing to create html
+    '''
+    print('welcome')
+
+add(13,31)
+message
